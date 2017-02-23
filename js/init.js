1bd232d7a0b5f3edc97c9255f0f49470351e9b39
Tofumaker <dyy2105@columbia.edu>
2017-02-22 16:44:08 -0800

    disable parallax for mobile

--- js/init.js
+++ js/init.js
@@ -12,82 +12,75 @@ $(function(){
   // Sticky Header
 
 $(window).scroll(function() {
+  if ($(window).scrollTop() > 150) {
+      $('.main_h').addClass('sticky');
+  } else {
+      $('.main_h').removeClass('sticky');
+  }
 
-    if ($(window).scrollTop() > 150) {
-        $('.main_h').addClass('sticky');
-    } else {
-        $('.main_h').removeClass('sticky');
-    }
+  if ($(window).scrollTop() > 150) {
+      $('.welcome-message').removeClass('welcome-hide');
+      $('.hero').addClass('blur-image');
+  } else {
+      $('.welcome-message').addClass('welcome-hide');
+      $('.hero').removeClass('blur-image');
+  }
 
-    if ($(window).scrollTop() > 150) {
-        $('.welcome-message').removeClass('welcome-hide');
-        $('.hero').addClass('blur-image');
-    } else {
-        $('.welcome-message').addClass('welcome-hide');
-        $('.hero').removeClass('blur-image');
-    }
+  if ($(window).scrollTop() > 400) {
+      $('#army-job').removeClass('welcome-hide');
+  } else {
+      $('#army-job').addClass('welcome-hide');
+  }
 
-    if ($(window).scrollTop() > 400) {
-        $('#army-job').removeClass('welcome-hide');
-    } else {
-        $('#army-job').addClass('welcome-hide');
-    }
+  if ($(window).scrollTop() > 480) {
+      $('#college-job').removeClass('welcome-hide');
+  } else {
+      $('#college-job').addClass('welcome-hide');
+  }
 
-    if ($(window).scrollTop() > 480) {
-        $('#college-job').removeClass('welcome-hide');
-    } else {
-        $('#college-job').addClass('welcome-hide');
-    }
+  if ($(window).scrollTop() > 590) {
+      $('#marketer-job').removeClass('welcome-hide');
+  } else {
+      $('#marketer-job').addClass('welcome-hide');
+  }
 
-    if ($(window).scrollTop() > 590) {
-        $('#marketer-job').removeClass('welcome-hide');
-    } else {
-        $('#marketer-job').addClass('welcome-hide');
-    }
+  if ($(window).scrollTop() > 650) {
+      $('#programmer-job').removeClass('welcome-hide');
+  } else {
+      $('#programmer-job').addClass('welcome-hide');
+  }
 
-    if ($(window).scrollTop() > 650) {
-        $('#programmer-job').removeClass('welcome-hide');
-    } else {
-        $('#programmer-job').addClass('welcome-hide');
-    }
-    
-    if ($(window).scrollTop() > 900) {
-        $('.projects-welcome-section').removeClass('welcome-hide');
-    } else {
-        $('.projects-welcome-section').addClass('welcome-hide');
-    }
+  if ($(window).scrollTop() > 900) {
+      $('.projects-welcome-section').removeClass('welcome-hide');
+  } else {
+      $('.projects-welcome-section').addClass('welcome-hide');
+  }
 
-    var navheight = $('.main_h').height();
+  var navheight = $('.main_h').height();
 
-    if ($(window).scrollTop() >= $('#bio-section').offset().top-navheight) {
-      $('.biography').addClass('lock-section');
-      $('.biography').addClass('lock-bio');
-      $('.biography').css('top', navheight);
-    } else{
-      $('.biography').removeClass('lock-section');
-      $('.biography').removeClass('lock-bio');
-    }
-    if ($(window).scrollTop() >= $('#projects-section').offset().top-navheight) {
-      $('.proyectos').addClass('lock-section');
-      $('.proyectos').css('top', navheight);
-    } else{
-      $('.proyectos').removeClass('lock-section');
-    }
+  if ($(window).scrollTop() >= $('#bio-section').offset().top-navheight) {
+    $('.biography').addClass('lock-section');
+    $('.biography').addClass('lock-bio');
+    $('.biography').css('top', navheight);
+  } else{
+    $('.biography').removeClass('lock-section');
+    $('.biography').removeClass('lock-bio');
+  }
+  
+  if ($(window).scrollTop() >= $('#projects-section').offset().top-navheight) {
+    $('.proyectos').addClass('lock-section');
+    $('.proyectos').css('top', navheight);
+  } else{
+    $('.proyectos').removeClass('lock-section');
+  }
 
-    if ($(window).scrollTop() >= $('#contacts-section').offset().top-navheight) {
-      $('.contacts-section').addClass('lock-section');
-      $('.contacts-section').css('top', navheight);
-    } else {
-      $('.contacts-section').removeClass('lock-section');
-    }
+  if ($(window).scrollTop() >= $('#contacts-section').offset().top-navheight) {
+    $('.contacts-section').addClass('lock-section');
+    $('.contacts-section').css('top', navheight);
+  } else {
+    $('.contacts-section').removeClass('lock-section');
+  }
 });
-
-// $('.card').mouseover(function(){
-//   $(this).addClass('hover-project');
-// });
-// $('.card').mouseout(function(){
-//   $(this).removeClass('hover-project');
-// });
 
 // Mobile Navigation
 $('.mobile-toggle').click(function() {
